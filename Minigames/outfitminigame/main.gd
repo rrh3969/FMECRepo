6b7d07dfcc3b485187aff3906965cd7e86e27979
rrh3969 <rrh3969@rit.edu>
2025-09-04 16:12:50 -0400

    yay prototype

--- Minigames/outfitminigame/main.gd
+++ Minigames/outfitminigame/main.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
-var outfitsections = []
+@export var outfits = []
 var correctOutfit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	correctOutfit = randi_range(0,2)
+	$Sprite2D.texture = outfits[correctOutfit]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_submit_button_pressed() -> void:
+	if $Outfitsection.currentOutfit == correctOutfit && $Outfitsection2.currentOutfit == correctOutfit && $Outfitsection3.currentOutfit == correctOutfit:
+		print("win")
+	else:
+		print("nope")
