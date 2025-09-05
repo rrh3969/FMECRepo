ea03cc720784adf15d1eb5d36f30d1c8d0dc8b4a
rrh3969 <rrh3969@rit.edu>
2025-09-05 16:58:50 -0400

    yay scoring

--- Minigames/outfitminigame/main.gd
+++ Minigames/outfitminigame/main.gd
@@ -2,20 +2,38 @@ extends Node2D
 
 @export var outfits = []
 var correctOutfit
+var gameWon = false
+var inGameTime
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	correctOutfit = randi_range(0,2)
 	$Sprite2D.texture = outfits[correctOutfit]
+	inGameTime = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	inGameTime += delta
+	
+	if !gameWon:
+		$Timer.text = "Time: " + str(roundf(inGameTime))
+		$TimerEnd.text = "Time: " + str(roundf(inGameTime))
+	else:
+		for child in get_children():
+			child.visible = false
+			$TimerEnd.visible = true
+			$ResetButton.visible = true
 
 
 func _on_submit_button_pressed() -> void:
 	if $Outfitsection.currentOutfit == correctOutfit && $Outfitsection2.currentOutfit == correctOutfit && $Outfitsection3.currentOutfit == correctOutfit:
 		print("win")
+		gameWon = true
 	else:
 		print("nope")
+
+
+func _on_reset_button_pressed() -> void:
+	get_tree().reload_current_scene()
+	print("reset")
